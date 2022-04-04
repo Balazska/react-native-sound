54e93ead7880df7a7e10358d6ff2ea4898ea61b5
Balázs Fodor <balazs.fodor@itsquad.hu>
2022-04-04 17:51:53 +0200

    set back to may duck

--- android/src/main/java/com/zmxv/RNSound/RNSoundModule.java
+++ android/src/main/java/com/zmxv/RNSound/RNSoundModule.java
@@ -248,7 +248,7 @@ public class RNSoundModule extends ReactContextBaseJavaModule implements AudioMa
       AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
 
       //audioManager.requestAudioFocus(this, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN);
-      audioManager.requestAudioFocus(this, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK);
+      audioManager.requestAudioFocus(this, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT_MAY_DUCK);
 
       this.focusedPlayerKey = key;
     }
